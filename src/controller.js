a8bcc843ecc7da2e7246cf80b71b52fe399c8ad0
root <kmtopchiy@gmail.com>
2019-08-19 23:08:28 +0300

    added multiplatform

---
+++ src/controller.js
@@ -0,0 +1,52 @@
+import Model from "./model";
+import ControllerCrud from "./controller/controllerCrud"
+import { initPaint } from "./controller/controllerPaint"
+import DKBankController from "./controller/controllerDkBank"
+export default class Controller {
+
+    model = new Model();
+    controllerCrud = new ControllerCrud();
+    controllerDKBank = new DKBankController();
+
+    insertToBodyBlock = (where, block) => {
+        return where.innerHTML = block;
+    };
+
+    isInputAdmin = (value) => {
+        return value === 'admin';
+    };
+
+    activateLogin = () => {
+        const crudLoginInput = document.getElementById('crudLoginInput');
+        const crudPasswordInput = document.getElementById('crudPasswordInput');
+        const btnCrudLogin = document.getElementById('btnCrudLogin');
+        const errorBox = document.getElementById('errorBox');
+        let containerBody = document.getElementById('containerBody');
+        const authorisationError = document.getElementById('authorisationError');
+
+        btnCrudLogin.addEventListener('click', (e)=> {
+            if ((this.isInputAdmin(crudLoginInput.value) === true) && (this.isInputAdmin(crudPasswordInput.value) === true)) {
+                 this.insertToBodyBlock(containerBody, this.model.containerBodyCrud);
+                return this.activateCrud();
+
+            } else {
+                console.log("show tip here");
+                this.insertToBodyBlock(errorBox, this.model.authorisationError);
+            }
+        })
+
+    };
+
+    activateDKbank = () => {
+        this.controllerDKBank.init();
+    };
+
+    activateCrud = () => {
+        this.controllerCrud.initCrud();
+    };
+
+    activatePaint = () => {
+        initPaint();
+    }
+
+}
